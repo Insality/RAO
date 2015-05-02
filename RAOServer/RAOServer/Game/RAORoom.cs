4924f9189587c0e199e3a0706bb3898d58ca2d3a
Insality <insality@gmail.com>
2015-05-02 13:31:28 +0600

    Обновлены файлы решения

--- RAOServer/RAOServer/Game/RAORoom.cs
+++ RAOServer/RAOServer/Game/RAORoom.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using RAOServer.Utils;
 
 namespace RAOServer.Game {
     /// <summary>
-    /// RAORoom - игровая комната одной арены. Создается при создании лобби
-    /// и обрабатывает все действия в этой комнате
+    ///     RAORoom - игровая комната одной арены. Создается при создании лобби
+    ///     и обрабатывает все действия в этой комнате
     /// </summary>
-    class RAORoom {
-        private Map map = new Map();
+    internal class RAORoom {
+        private readonly Map map = new Map();
 
         public RAORoom() {
             map.LoadMapFromFile("testMap.txt");
